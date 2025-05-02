4a3c6f275a4141d234b31b496da6d768c7a97885
Alvin Anil <84265395+Alvin4646@users.noreply.github.com>
2025-05-02 19:53:43 +0530

    Initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,54 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
+import { getLoggedInUser } from '@/lib/actions/user.actions';
+import {getAccount, getAccounts} from "@/lib/actions/bank.actions";
+import RecentTransactions from "@/components/RecentTransactions";
+
+const Home = async ({searchParams:{id,page}}:SearchParamProps) => {
+    const currentPage=Number(page as string) || 1
+  const loggedIn = await getLoggedInUser();
+  console.log(loggedIn)
+    const accounts=await getAccounts({userId:loggedIn.$id });
+    // if(!accounts) return ;
+
+    const accountsData=accounts?.data;
+    const appwriteItemId=(id as string)|| accountsData[0]?.appwriteItemId;
+    const account =await  getAccount(appwriteItemId);
+    console.log(accounts?.transactions);
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox 
+            accounts={accountsData}
+            totalBanks={accounts?.totalBanks}
+            totalCurrentBalance={accounts?.totalCurrentBalance}
+          />
+        </header>
+
+        <RecentTransactions
+          accounts={accountsData}
+          transactions={accounts?.transactions}
+          appwriteItemId={appwriteItemId}
+          page={currentPage}
+        />
+      </div>
+
+      <RightSidebar 
+        user={loggedIn}
+        transactions={account?.transactions}
+        banks={accountsData?.slice(0, 2)}
+      />
+    </section>
+  )
+}
+
+export default Home
---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://b9a18951e63fb77d5b3be6dc86e1eba5@o4509119307710464.ingest.us.sentry.io/4509119309807616",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
